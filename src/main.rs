9c954bb6ef84ed7e5f2766d3c304be7355ecaf1d
Vedh Kumar <vedhkumar237@gmail.com>
2025-06-30 23:19:14 +0530

    sign message

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,20 @@ use axum::{
 };
 mod create_token;
 mod keypair;
+mod sign_message;
 
 use create_token::create_token;
 use keypair::generate_keypair;
+use sign_message::sign_message;
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new()
-        .route("/", get(generate_keypair))
-        .route("/token/create", post(create_token));
+        .route("/keypair", get(generate_keypair))
+        .route("/token/create", post(create_token))
+        .route("/message/sign", post(sign_message));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
-// use axum::{
-//     Router,
-//     routing::{get, post},
-// };
-// mod keypair;
-// mod sign_message;
-// #[tokio::main]
-// async fn main() {
-//     let app = Router::new()
-//         .route("/keypair", post(keypair::generate_keypair))
-//         // .route("/token/create", post(create_token::create_token))
-//         // .route("/message/sign", post(sign_message::sign_message));
-
-//     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-//     axum::serve(listener, app).await.unwrap();
-// }
--- src/sign_message.rs
+++ src/sign_message.rs
@@ -1,18 +1,49 @@
-// use axum::Json;
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Json},
+};
 
-// struct Request {
-//     message: String,
-//     secret: String,
-// }
+use serde::{Deserialize, Serialize};
 
-// struct Response {
-//     success: bool,
-//     data: DataResponse,
-// }
+#[derive(Deserialize, Serialize)]
+pub struct ErrorResponse {
+    pub succes: bool,
+    pub error: String,
+}
+#[derive(Deserialize, Serialize)]
+pub struct Request {
+    message: String,
+    secret: String,
+}
+#[derive(Deserialize, Serialize)]
 
-// pub struct DataResponse {
-//     signature: String,
-//     pubkey: String,
-//     message: String,
-// }
-// pub async fn sign_message(Json(req): Json<Request>) -> Json<Response> {}
+pub struct Response {
+    success: bool,
+    data: DataResponse,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct DataResponse {
+    signature: String,
+    pubkey: String,
+    message: String,
+}
+pub async fn sign_message(Json(payload): Json<Request>) -> impl IntoResponse {
+    if bs58::decode(&payload.secret).into_vec().is_err() {
+        let error = ErrorResponse {
+            succes: false,
+            error: "Invalid base58 secret key".to_string(),
+        };
+        return (StatusCode::BAD_REQUEST, Json(error)).into_response();
+    }
+    let res = Response {
+        success: true,
+        data: DataResponse {
+            signature: "3xsPnrh3P7QjjwT1qRHTzLa7f3aWBCdyaAYh3HPmshKWVmFqMQjUMdL66Z3JhAFqG9Rw4uZnHFybZug3XGRXpNQ7"
+                .to_string(),
+            pubkey: "8G79rFduh5RMVZySsrJPqtgUKsZCnVJvDnQfHc4MPLAv".to_string(),
+            message: "Hello, Solana!".to_string(),
+        },
+    };
+    return (StatusCode::OK, Json(res)).into_response();
+}
